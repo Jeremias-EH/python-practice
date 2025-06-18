0fdd940c0fa987293962cefa0893bbf48e7f78b1
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-18 17:32:23 -0300

    Ejercicio 04: Concatenado o union de cadenas de texto

---
+++ Semana1/concatenar_strings.py
@@ -0,0 +1,11 @@
+"""
+Intentare concatenar o unir 2 palabras/cadenas de texto
+
+"""
+
+string1 = "Buenos"
+string2 = "Dias"
+
+concatenado = string1 + " " + string2 
+
+print("El concatenado es: " , concatenado)
