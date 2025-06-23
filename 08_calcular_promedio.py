760d0e0e2ba57ee37d89245562babc3be6db0204
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-23 17:38:37 -0300

    Ejercicio 08: Calculo de promedio numerico

---
+++ 08_calcular_promedio.py
@@ -0,0 +1,8 @@
+"""
+Creare una lista de numeros y luegos los voy a promediar
+"""
+
+numeros = [1,2,3,4,5,6,7,8,9]
+promedio = sum(numeros) / len(numeros)
+
+print("El promedio es: " , promedio , "!!")
