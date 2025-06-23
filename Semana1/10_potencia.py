a69f976d4bb67fb8f27c09c5cc6756a05edebd6c
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-23 17:51:24 -0300

    Ejercicio 10: Potencia de un numero

---
+++ Semana1/10_potencia.py
@@ -0,0 +1,10 @@
+"""
+Voy a realizar una potencia a un numero
+
+"""
+
+base = 5
+exponente = 5
+resultado = base ** exponente
+
+print("El resultado de la potencia de 5 elevado a 5 es: " , resultado)
