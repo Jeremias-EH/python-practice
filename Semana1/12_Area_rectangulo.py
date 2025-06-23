990cda4647a45a4f05cc86b19282d576bc45e403
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-23 18:22:10 -0300

    Ejercicio 12: Calculo de area de un rectangulo

---
+++ Semana1/12_Area_rectangulo.py
@@ -0,0 +1,10 @@
+"""
+Calculare el area de un rectangulo utilizando base y altura
+
+"""
+
+base = int(input("Ingrese la base: "))
+altura = int(input("Ingrese la altura: "))
+area = base * altura 
+
+print("El area es: " , area)
