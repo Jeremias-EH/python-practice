2a6fd35b3b065e96240940f1ffa754b7311c5370
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-23 18:45:58 -0300

    Ejercicio 13: Convertir numero a string

---
+++ Semana1/13_num_a_string.py
@@ -0,0 +1,9 @@
+"""
+Voy a convertir un numero a string/cadena de texto
+
+"""
+
+numero = int(input("Ingrese el numero a convertir: "))
+print("El numero a texto: " , type(numero))
+cadena = numero
+print("Ahora este texto!: " , cadena)
