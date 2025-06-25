2f9b0ffc079f6c5e984c522075356fd98cb9f02d
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-24 23:27:17 -0300

    Ejercicio 18: Extraer de forma selectiva partes de la cadena/string

---
+++ Semana1/18_subcadena.py
@@ -0,0 +1,8 @@
+"""
+Voy a extraer una cadena de una cadena, o sea, una subcadena
+"""
+
+string = "PYTHON"
+sub1 = string[0:3]
+sub2 = string[3:6]
+print("La cadena original es: " , string , "La primer subcadena es: " , sub1 , "Y la segunda subcadena es: " , sub2)
