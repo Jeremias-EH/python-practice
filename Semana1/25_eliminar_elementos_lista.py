2730c370ea2b316f8f7613deb5d72321a20ccfb6
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-27 23:40:30 -0300

    Ejercicio 25: Eliminacion de un elemento de una lista

---
+++ Semana1/25_eliminar_elementos_lista.py
@@ -0,0 +1,9 @@
+"""
+Voy a eliminar un elemento de una lista(Array)
+"""
+
+array = [1,2,3,4,5,6,7,8,9,10]
+
+array.remove(5)
+
+print("Resultado: " , array , ".Como puede ver, se elimino el numero 5")
