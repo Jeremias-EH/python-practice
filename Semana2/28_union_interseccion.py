81e7b1391dd2a626d25880576f846e4c095e4bb5
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-30 23:50:45 -0300

    Ejercicio 28: Union e Interseccion de conjuntos

---
+++ Semana2/28_union_interseccion.py
@@ -0,0 +1,11 @@
+"""
+voy a realizar una operacion basica de union e interseccion
+"""
+
+con1 = {1,2,3,4,5}
+con2 = {5,6,7,8,9,10}
+
+uni = con1 | con2 
+inter = con1 & con2 
+
+print("La union quedaria asi: " , uni , "y la interseccion seria: " , inter)
