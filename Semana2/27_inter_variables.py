8725011bcdc72cda0036b43291ad96cac8a15ce4
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-06-30 23:45:08 -0300

    Ejercicio 27: Intercambio de variables

---
+++ Semana2/27_inter_variables.py
@@ -0,0 +1,10 @@
+"""
+Intercambiare 2 variables con asignacion multiple
+"""
+
+var1 = 8
+var2 = 16
+
+var1,var2 = var2,var1
+
+print("La variable 1 originalmente era un 8 y la variable 2 era un 16, pero ahora la variable 1 es: ", var1 , "y la variable 2 es: ", var2)
