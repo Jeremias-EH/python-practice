8dfa65a760c4b98958e8f98ce8a4a4dc8d55979f
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-02 23:54:36 -0300

    Ejercicio 29: Extraccion de elemento de una tupla

---
+++ Semana2/29_extraer_elemento_tupla.py
@@ -0,0 +1,9 @@
+"""
+Extraere un elemento de una tupla
+"""
+
+tupla = (33,66,99)
+"""El indice "[]" empieza desde el 0, que seria el primer elemento,
+ en este caso, citare el segundo elemento, el 66"""
+elemento = tupla [1] 
+print("La tupla consta de; " , tupla , "Y el segundo elemento extraido seria: " , elemento)
