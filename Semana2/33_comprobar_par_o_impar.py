b5f75c1c567c1eb98f240c9044f7aaceeb889e92
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-03 23:16:23 -0300

    Ejercicio 33: Comprobacion de par o impar

---
+++ Semana2/33_comprobar_par_o_impar.py
@@ -0,0 +1,10 @@
+"""
+Voy a ingresar un numero y comprobare si es par o impar
+"""
+
+num1 = int(input("Ingrese el numero a comprobar: "))
+
+if num1 % 2 == 0:
+    print("Es Par")
+else:
+ print("Es impar")
