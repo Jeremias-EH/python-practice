671dda3e4f67f5264643b68540233ef4da88c86d
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-03 23:28:37 -0300

    Ejercicio 35: Comprobar cantidad de caracteres de una cadena

---
+++ Semana2/35_verificar_num_array.py
@@ -0,0 +1,10 @@
+"""
+Verificare si una cadena es mayor o igual a 10 caracteres
+"""
+
+array = "Hola que tal mundo!"
+
+if len(array) >= 10:
+    print("Se verifico que la cadena si tiene 10 o mas caracteres")
+else:
+    print("Se verifico que la cadena NO tiene 10 caracteres")
