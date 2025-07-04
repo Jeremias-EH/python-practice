3c82e8c1258bd6238464b6ca97e64cabdc583e71
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-03 23:31:46 -0300

    Ejercicio 36: Comprobar si un numero esta entre 1 y 100

---
+++ Semana2/36_rango_1_100.py
@@ -0,0 +1,10 @@
+"""
+Comprobare si el numero ingresado esta entre 1 y 100
+"""
+
+num1 = int(input("Ingrese el numero a comprobar: "))
+
+if num1 >= 1 and num1 <= 100:
+    print("Esta dentro del rango de 1 al 100")
+else:
+    print("Esta fuera del rango designado")
