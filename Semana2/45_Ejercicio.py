6996d24c00c74f6d3d65e6cec34c7c42d85c3abb
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-09 00:07:05 -0300

    Ejercicio 45: Juego de numero aleatorio

---
+++ Semana2/45_Ejercicio.py
@@ -0,0 +1,16 @@
+"""
+Generare un numero aleatorio entre 1 y 10.
+Luego se pedira al usuario que ingrese numeros hasta que acierte.
+"""
+
+import random
+
+num_random = random.randint(1, 10)
+intentos = 0
+
+while True:
+    num1 = int(input("Adivina el numero...: "))
+    intentos = intentos + 1
+    if num1 == num_random:
+        print(f"Eres genial, adivinaste. Esto te tomo: " , intentos , " Intentos hasta que acertaste!")
+        break
