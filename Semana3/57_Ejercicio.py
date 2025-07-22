54a2deb1cf8960ba16b325157212c4f2cf913e4c
Jeremias Emanuel Hernandez <jeremiashernandez541@gmail.com>
2025-07-21 23:48:46 -0300

    Ejercicio 57: Listar 10 numeros y calcular su cuadrado usando for

---
+++ Semana3/57_Ejercicio.py
@@ -0,0 +1,8 @@
+"""
+listar 10 numeros y calcular el cuadrado de cada uno
+ e imprimirlo utilizando for
+"""
+
+for i in range(1,11):
+    cuadrado = i ** 2
+    print("El cuadrado de: ", i , " Es: " , cuadrado)
